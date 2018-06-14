347e76ea60b1f9085589090eecdc0d1cad67635c
Koen Dejonghe <koen.dejonghe@cegeka.com>
2018-06-14 16:10:15 +0200

    adding scipt for windows

---
+++ jni-builder/build-jni-cpu-win.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -e
+
+function build_aten_lib() {
+  # if [ ! -d build ] 
+  # then
+    mkdir -p build
+    cd build
+    cmake .. -DCMAKE_INSTALL_PREFIX=../target -DNO_CUDA=true
+    make install
+    cd ..
+  # else
+    # echo "build directory exists, skipping ATen build..."
+  # fi
+}
+
+function build_jni_lib() {
+  echo "preprocessing header files"
+  cd target
+  cp -R include include-swig
+  cd include
+  for f in $(find . -name \*.h); do
+      cat $f \
+      | sed -E "s|<TH(.*)>|\"TH\1\"|g" \
+      | grep -v "#include <.*>" \
+      | grep -v "#include \"cu.*\.h\"" \
+      | grep -v "TH_NO_RETURN" \
+      | sed -e "s|__thalign__([0-9])||g" \
+      > ../include-swig/$f
+  done
+  cd ..
+
+  cp ../jni-builder/torch-cpu.h include-swig
+  cd include-swig
+  cc -P -E -I TH -I THNN -I THS torch-cpu.h > torch-cpu-preprocessed.h
+
+  echo "swigging"
+  cd ..
+  mkdir -p java/src/main/java/torch/cpu
+  cp ../jni-builder/torch-cpu.i .
+  swig -java -package torch.cpu -outdir java/src/main/java/torch/cpu torch-cpu.i
+
+  echo "compiling swig wrapper"
+  cc -c torch-cpu_wrap.c \
+    -I $JAVA_HOME/include \
+    -I $JAVA_HOME/include/win32 \
+    -I include/TH \
+    -I include/THNN \
+    -I include/THS
+
+  echo "building dynamic library"
+  # cc -dynamiclib -undefined suppress -flat_namespace torch-cpu_wrap.o -o lib/libjnitorchcpu.dylib
+  cc -shared torch-cpu_wrap.o -o lib/jnitorchcpu.dll
+
+  cd ..
+}
+
+function package {
+  cp jni-builder/build.sbt target
+  cd target
+  sbt java/publishLocal
+}
+
+build_aten_lib
+build_jni_lib
+package
+
